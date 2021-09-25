187e64e279784b5de8d41278ecd170970e6f30ed
vishaak.m <vishaak.m@gmail.com>
2021-09-25 19:33:03 +0530

    First Draft

---
+++ res/js/data.js
@@ -0,0 +1,55 @@
+const images = [
+    {
+        name: 'Example',
+        uri: 'minion.jpg',
+        desc: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+        name: 'Example',
+        uri: 'minion.jpg',
+        desc: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+        name: 'Example',
+        uri: 'minion.jpg',
+        desc: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+        name: 'Example',
+        uri: 'minion.jpg',
+        desc: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+        name: 'Example',
+        uri: 'minion.jpg',
+        desc: 'Lorem ipsum dolor sit amet.',
+    },
+];
+
+const videos = [
+    {
+        name: 'Example',
+        uri: 'https://www.youtube.com/embed/LDU_Txk06tM',
+        desc: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+        name: 'Example',
+        uri: 'https://www.youtube.com/embed/LDU_Txk06tM',
+        desc: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+        name: 'Example',
+        uri: 'https://www.youtube.com/embed/LDU_Txk06tM',
+        desc: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+        name: 'Example',
+        uri: 'https://www.youtube.com/embed/LDU_Txk06tM',
+        desc: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+        name: 'Example',
+        uri: 'https://www.youtube.com/embed/LDU_Txk06tM',
+        desc: 'Lorem ipsum dolor sit amet.',
+    }
+];
